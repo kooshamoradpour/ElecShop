43d792865f898466f74113dd7c5fcd438b1e8405
Philip Ecdao <phil@Philips-MacBook-Air.local>
2025-03-15 13:14:41 -0400

    worked on schemas, models

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,4 +1,4 @@
-import { User } from '../models/index.js';
+import { Product, User } from '../models/index.js';
 import { signToken, AuthenticationError } from '../utils/auth.js'; 
 
 // Define types for the arguments
@@ -15,19 +15,19 @@ interface LoginUserArgs {
   password: string;
 }
 
-interface UserArgs {
-  username: string;
-}
+// interface UserArgs {
+//   username: string;
+// }
 
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
-    },
-    user: async (_parent: any, { username }: UserArgs) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
+    // users: async () => {
+      // return User.find().populate('thoughts');
+    // },
+    // user: async (_parent: any, { username }: UserArgs) => {
+    //   return User.findOne({ username }).populate('thoughts');
+    // },
 
     // Query to get the authenticated user's information
     // The 'me' query relies on the context to check if the user is authenticated
@@ -39,6 +39,10 @@ const resolvers = {
       // If the user is not authenticated, throw an AuthenticationError
       throw new AuthenticationError('Could not authenticate user.');
     },
+    product: async (_parent: any,{ name }: any, _context: any) => {
+      const product = await Product.findOne({ name });
+      return product
+    }
   },
   Mutation: {
     addUser: async (_parent: any, { input }: AddUserArgs) => {
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -2,17 +2,47 @@ import gql from 'graphql-tag';
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    cart: Cart
   }
+  type Product{
+    productId: String!
+    name: String!
+    description: String!
+    image: String!
+    price: Float!
+    quantity: Int!
+    # link: String!
+}
+type CartItem{
+  product: Product!
+  quantity: Int !
+  
+}
+type Cart {
+    _id: ID
+    user: User
+    items: [CartItem]
+    # link: String!
+}
 
   input UserInput {
     username: String!
     email: String!
     password: String!
   }
+
+  input SaveProduct {
+  productId: String!
+  name: String!
+  description: String!
+  image: String!
+  price: Float!
+  quantity: Int!
+}
   
   type Auth {
     token: ID!
@@ -20,14 +50,18 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
+    # users: [User]
+    # user(username: String!): User
     me: User
+    product: Product
   }
 
   type Mutation {
     addUser(input: UserInput!): Auth
     login(email: String!, password: String!): Auth
+    saveProductToCart(input: saveProduct!):User
+    removeProductFromCart(productId: String!):User
+    addProductToDB(input: saveProduct): Product
   }
 `;
 
